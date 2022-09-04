342c2ffc20b44bd81fe3e775e74480d13beb43f9
Makar Timofeev <75175813+brintadis@users.noreply.github.com>
2022-09-04 13:35:36 +0300

    delete playlist, delete track, playlist detail view added

--- any_sync/playlist/urls.py
+++ any_sync/playlist/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 
-from .views import show_playlist
+from .views import show_playlist, delete_playlist, delete_track
 
 urlpatterns = [
-    path("<int:playlist_id>/", show_playlist, name="playlist"),
+    path("<int:playlist_id>/", show_playlist, name="playlist_detail"),
+    path(
+        "delete-playlist/<int:playlist_id>/<int:user_id>/",
+        delete_playlist,
+        name="delete_playlist"
+    ),
+    path(
+        "delete-track/<int:track_id>/<int:playlist_id>/",
+        delete_track,
+        name="delete_track"
+    ),
 ]
--- any_sync/playlist/views.py
+++ any_sync/playlist/views.py
@@ -1,5 +1,27 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from .models import Playlist, Track
 
 
 def show_playlist(request, playlist_id):
-    return HttpResponse(f"Playlist with id = {playlist_id}")
+    current_playlist = Playlist.objects.filter(id=playlist_id).first()
+    tracks = Track.objects.filter(playlist=playlist_id).all()
+
+    context = {
+        'current_playlist': current_playlist,
+        'tracks': tracks,
+    }
+
+    return render(request, 'playlist/playlist_detail.html', context=context)
+
+
+def delete_playlist(request, playlist_id, user_id):
+    Playlist.objects.filter(id=playlist_id).delete()
+
+    return redirect('profile', user_id=user_id)
+
+
+def delete_track(request, track_id, playlist_id):
+    Track.objects.filter(id=track_id).delete()
+
+    return redirect('playlist_detail', playlist_id=playlist_id)
