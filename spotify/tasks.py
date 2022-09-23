1a006cd3ea50fc8114c91c0e61d99016952d02d2
Makar Timofeev <75175813+brintadis@users.noreply.github.com>
2022-09-23 18:05:15 +0300

    Spotify Celery task docstring added

--- spotify/tasks.py
+++ spotify/tasks.py
@@ -5,6 +5,16 @@ from spotify.spotify import SyncSpotifyPlaylists
 
 @shared_task
 def create_playlists_spotify(user_id, playlist_ids, visibility):
+    """Creating playlists using spotipy and celery
+
+    Args:
+        user_id (`int`): current user id
+        playlist_ids (`list`): list of playlist ids to user while creating spotify playlists
+        visibility (`Bool`): make a public or a private playlist
+
+    Returns:
+        `list`: list of skipped tracks while search using the music service
+    """
     sync_playlist = SyncSpotifyPlaylists(
         user_id=user_id,
         playlist_ids=playlist_ids,
