7eaccb9933945b077713aea7dd742dbd35ec0a23
Makar Timofeev <75175813+brintadis@users.noreply.github.com>
2022-10-08 20:42:27 +0300

    celery yandex create playlist docsting added

--- ya_music/tasks.py
+++ ya_music/tasks.py
@@ -5,6 +5,13 @@ from .ya_music import SyncYandexPlaylists
 
 @shared_task
 def create_playlists_yandex(token, playlist_ids, visibility):
+    """Create yandex music playlist using Celery
+
+    Args:
+        token (`str`): yandex api token
+        playlist_ids (`list`): DB playlist ids
+        visibility (`str`): make a playlist public or private
+    """
     sync_playlist = SyncYandexPlaylists(
         token=token,
         playlist_ids=playlist_ids,
